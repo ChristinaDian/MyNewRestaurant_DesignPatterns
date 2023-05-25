91b19317da6f62bf3f6b4d22679088348a3b673c
ChristinaDian <krisdian2001@gmail.com>
2023-05-25 21:25:17 +0300

    <Main method added>

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import command.Cashier;
+import command.Chef;
+import decorator.GarlicSauce;
+import decorator.Ketchup;
+import factorymethod.Burger;
+import factorymethod.KitchenFactory;
+import singleton.RestaurantSingleton;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        RestaurantSingleton restaurant = RestaurantSingleton.getInstance();
+
+        restaurant.welcomeCustomer();
+
+        Chef chef = new Chef();
+        Cashier cashier = new Cashier(chef);
+
+        cashier.takeOrder("Classic");
+        cashier.takeOrder("Vegetarian");
+
+        Burger burger = KitchenFactory.makeBurger("King");
+        burger.make();
+        burger.addSauce(new Ketchup(burger));
+        burger.addSauce(new GarlicSauce(burger));
+        burger.serve();
+
+        cashier.undoLastOrder();
     }
 }
